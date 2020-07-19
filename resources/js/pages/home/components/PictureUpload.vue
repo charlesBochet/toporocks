334993fee1373bd8fdcbc8aec0604517de16389e
Charles Bochet <charles.bochet@ext.airbnb.com>
2020-07-19 10:51:15 +0200

    Add image resize

--- resources/js/pages/home/components/PictureUpload.vue
+++ resources/js/pages/home/components/PictureUpload.vue
@@ -1,16 +1,28 @@
 <template>
   <div ref="picture-container">
-    <input type="file" accept="image/png, image/jpeg" class="image-input" @change="loadPicture">
+    <image-uploader
+      :debug="1"
+      :max-width="1024"
+      :quality="0.7"
+      output-format="verbose"
+      :preview="false"
+      :class-name="['fileinput']"
+      :capture="false"
+      accept="video/*,image/*"
+      do-not-resize="['gif', 'svg']"
+      @input="loadPicture"
+    />
     <editor v-if="displayEditor" ref="editor" :canvas-height="canvasHeight" :canvas-width="canvasWidth" />
   </div>
 </template>
 
 <script>
 import Editor from 'vue-image-markup'
+import ImageUploader from 'vue-image-upload-resize'
 
 export default {
   name: 'PictureUpload',
-  components: { Editor },
+  components: { Editor, ImageUploader },
   data () {
     return {
       canvasHeight: 150,
@@ -22,35 +34,33 @@ export default {
     }
   },
   methods: {
-    loadPicture (e) {
+    loadPicture (base64image) {
       this.displayEditor = false
-      const image = e.target.files[0]
+      fetch(base64image.dataUrl).then(res => {
+        res.blob().then(image => {
+          let data = new FormData()
+          data.append('file', image)
+          this.uploadConfig = {
+            header: {
+              'Content-Type': base64image.info.type
+            }
+          }
+          return this.$http.post('api/pictures/upload', data, this.uploadConfig).then((response) => {
+            this.canvasWidth = this.$refs['picture-container'].clientWidth
+            let image = new Image()
+            image.src = response.data.url
+            image.onload = () => {
+              this.canvasHeight = this.canvasWidth / image.width * image.height
+              this.displayEditor = true
+              this.$nextTick(() => {
+                this.$refs.editor.set('freeDrawing', { stroke: 'red' })
+                this.$refs.editor.setBackgroundImage(response.data.url)
+              })
+            }
 
-      let data = new FormData()
-      data.append('file', image)
-
-      this.uploadConfig = {
-        header: {
-          'Content-Type': e.target.mime
-        }
-      }
-      this.$http.post('api/pictures/upload', data, this.uploadConfig).then((response) => {
-        this.canvasWidth = this.$refs['picture-container'].clientWidth
-        let image = new Image()
-        image.src = response.data.url
-        image.onload = () => {
-          this.canvasHeight = this.canvasWidth / image.width * image.height
-          this.displayEditor = true
-          this.$nextTick(() => {
-            this.$refs.editor.set('freeDrawing', { stroke: 'red' })
-            this.$refs.editor.setBackgroundImage(response.data.url)
+            this.picturePath = response.data.path
           })
-        }
-
-        this.picturePath = response.data.path
-      }).catch((error) => {
-        alert('Error while uploading')
-        console.log(error)
+        })
       })
     },
     savePictures () {
